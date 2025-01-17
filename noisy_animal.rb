2819f84b2713f30fe985a7a45ac4a5bd95c02e81
Fritz Meissner <fritz.meissner@gmail.com>
2025-01-17 14:16:30 +0000

    Up the ugliness for an additional challenge

--- noisy_animal.rb
+++ noisy_animal.rb
@@ -5,16 +5,56 @@ class NoisyAnimal
     @species = species
   end
 
-  def make_noise_twice
-    if species == "cat"
-      puts "meow"
+  def make_noise(is_loud = true)
+    if is_bird && !is_loud
+      make_bird_noise(is_loud: false)
+    elsif is_bird
+      make_bird_noise(is_loud: true)
+    end
+    if is_loud
+      if species == "cat"
+        puts "meow"
+        puts "meow"
+      elsif species == "dog"
+        puts "woof"
+        puts "woof"
+      elsif species == "leopard"
+        puts "growl"
+        puts "growl"
+      end
+    elsif species == "cat"
       puts "meow"
     elsif species == "dog"
       puts "woof"
-      puts "woof"
     elsif species == "leopard"
       puts "growl"
-      puts "growl"
     end
+  end
+
+  private
+
+  def make_bird_noise(is_loud: true)
+    if species == "hadedah"
+      puts "squawk"
+    elsif species == "eagle"
+      puts "caw"
+    else
+      puts "hoot"
+    end
+    if is_loud
+      if species == "owl"
+        puts "hoot"
+      end
+      if species == "eagle"
+        puts "caw"
+      end
+      if species == "hadedah"
+        puts "squawk"
+      end
+    end
+  end
+
+  def is_bird
+    species == "owl" || species == "eagle" || species == "hadedah"
   end
 end
--- spec/noisy_animal_spec.rb
+++ spec/noisy_animal_spec.rb
@@ -6,7 +6,7 @@ describe NoisyAnimal do
     it "makes a meow noise twice" do
       animal = NoisyAnimal.new("cat")
 
-      expect { animal.make_noise_twice }.to output("meow\nmeow\n").to_stdout
+      expect { animal.make_noise }.to output("meow\nmeow\n").to_stdout
     end
   end
 
@@ -14,7 +14,7 @@ describe NoisyAnimal do
     it "makes a woof noise twice" do
       animal = NoisyAnimal.new("dog")
 
-      expect { animal.make_noise_twice }.to output("woof\nwoof\n").to_stdout
+      expect { animal.make_noise }.to output("woof\nwoof\n").to_stdout
     end
   end
 
@@ -22,7 +22,65 @@ describe NoisyAnimal do
     it "makes a growl noise twice" do
       animal = NoisyAnimal.new("leopard")
 
-      expect { animal.make_noise_twice }.to output("growl\ngrowl\n").to_stdout
+      expect { animal.make_noise }.to output("growl\ngrowl\n").to_stdout
+    end
+  end
+
+  context "when the animal is unusually quiet" do
+    it "cat goes meow" do
+      animal = NoisyAnimal.new("cat")
+
+      expect { animal.make_noise(false) }.to output("meow\n").to_stdout
+    end
+
+    it "dog goes woof" do
+      animal = NoisyAnimal.new("dog")
+
+      expect { animal.make_noise(false) }.to output("woof\n").to_stdout
+    end
+
+    it "leopard goes growl" do
+      animal = NoisyAnimal.new("leopard")
+
+      expect { animal.make_noise(false) }.to output("growl\n").to_stdout
+    end
+  end
+
+  context "when the animal is really a bird" do
+    it "loud owls hoot twice" do
+      animal = NoisyAnimal.new("owl")
+
+      expect { animal.make_noise(true) }.to output("hoot\nhoot\n").to_stdout
+    end
+
+    it "loud eagles caw twice" do
+      animal = NoisyAnimal.new("eagle")
+
+      expect { animal.make_noise(true) }.to output("caw\ncaw\n").to_stdout
+    end
+
+    it "loud hadedahs squawk twice" do
+      animal = NoisyAnimal.new("hadedah")
+
+      expect { animal.make_noise(true) }.to output("squawk\nsquawk\n").to_stdout
+    end
+
+    it "quiet owls hoot" do
+      animal = NoisyAnimal.new("owl")
+
+      expect { animal.make_noise(false) }.to output("hoot\n").to_stdout
+    end
+
+    it "quiet eagles caw" do
+      animal = NoisyAnimal.new("eagle")
+
+      expect { animal.make_noise(false) }.to output("caw\n").to_stdout
+    end
+
+    it "quiet hadedahs still squawk" do
+      animal = NoisyAnimal.new("hadedah")
+
+      expect { animal.make_noise(false) }.to output("squawk\n").to_stdout
     end
   end
 end
