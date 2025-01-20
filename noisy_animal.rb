168dd60816a60da0823e5ed6f4bf86decedacc90
Fritz Meissner <fritz.meissner@gmail.com>
2025-01-20 17:15:33 +0000

    Remove wrappers except for WithoutQuietVoice

--- noisy_animal.rb
+++ noisy_animal.rb
@@ -16,46 +16,39 @@ class NoisyAnimal
 end
 
 class Animal
+  attr_accessor :quiet_noise, :loud_noise
+
   def self.from_species(species)
     case species
     when 'hadedah'
-      WithLoudVoice.new(WithoutQuietVoice.new(Animal.new), %w[squawk squawk])
+      WithoutQuietVoice.new(Animal.new('', %w[squawk squawk]))
     when 'dog'
-      WithLoudVoice.new(WithQuietVoice.new(Animal.new, 'woof'), %w[woof woof])
+      Animal.new('woof', %w[woof woof])
     when 'leopard'
-      WithLoudVoice.new(WithQuietVoice.new(Animal.new, 'growl'), %w[growl growl])
+      Animal.new('growl', %w[growl growl])
     when 'cat'
-      WithLoudVoice.new(WithQuietVoice.new(Animal.new, 'meow'), %w[meow meow])
+      Animal.new('meow', %w[meow meow])
     when 'eagle'
-      WithLoudVoice.new(WithQuietVoice.new(Animal.new, 'caw'), %w[caw caw])
+      Animal.new('caw', %w[caw caw])
     when 'owl'
-      WithLoudVoice.new(WithQuietVoice.new(Animal.new, 'hoot'), %w[hoot hoot])
+      Animal.new('hoot', %w[hoot hoot])
     when 'snake'
-      WithLoudVoice.new(WithQuietVoice.new(Animal.new, 'slither'), 'hiss')
+      Animal.new('slither', 'hiss')
     when 'mouse'
-      WithLoudVoice.new(WithQuietVoice.new(Animal.new, ''), '')
+      Animal.new('', '')
     end
   end
-end
 
-class WithQuietVoice < SimpleDelegator
-  def initialize(obj, quiet)
-    super(obj)
-    @quiet = quiet
+  def initialize(quiet_noise = '', loud_noise = '')
+    @quiet_noise = quiet_noise
+    @loud_noise = loud_noise
   end
 
-  def quiet = puts @quiet
+  def quiet = puts quiet_noise
+
+  def loud = puts loud_noise
 end
 
 class WithoutQuietVoice < SimpleDelegator
   def quiet = raise "no such thing!"
-end
-
-class WithLoudVoice < SimpleDelegator
-  def initialize(obj, loud)
-    super(obj)
-    @loud = loud
-  end
-
-  def loud = puts @loud
 end
