a63efc891697285b2e6b6116210605cf1bf877eb
Fritz Meissner <fritz.meissner@gmail.com>
2025-02-21 17:07:54 +0000

    DRY up the ternary and invocation

--- javascript/noisy_animal.js
+++ javascript/noisy_animal.js
@@ -8,54 +8,60 @@ function createNoisyAnimal(species) {
     console.log(sound);
   }
 
-  function makeNoise({ loud = true } = {}) {
-    switch (species) {
+function makeNoise({ loud = true } = {}) {
+  let actions;
+
+  switch (species) {
     case 'cat':
-      ({
+      actions = {
         loud: () => loudSound('meow'),
         quiet: () => quietSound('meow')
-      })[loud ? 'loud' : 'quiet']();
+      };
       break;
 
     case 'dog':
-      ({
+      actions = {
         loud: () => loudSound('woof'),
         quiet: () => quietSound('woof')
-      })[loud ? 'loud' : 'quiet']();
+      };
       break;
 
     case 'leopard':
-      ({
+      actions = {
         loud: () => loudSound('growl'),
         quiet: () => quietSound('growl')
-      })[loud ? 'loud' : 'quiet']();
+      };
       break;
 
     case 'hadedah':
-      ({
+      actions = {
         loud: () => loudSound('squawk'),
         quiet: () => {
           quietSound('squawk');
           throw new Error('there is no such thing as a quiet hadedah!');
         }
-      })[loud ? 'loud' : 'quiet']();
+      };
       break;
 
     case 'eagle':
-      ({
+      actions = {
         loud: () => loudSound('caw'),
         quiet: () => quietSound('caw')
-      })[loud ? 'loud' : 'quiet']();
+      };
       break;
 
     case 'owl':
-      ({
+      actions = {
         loud: () => loudSound('hoot'),
         quiet: () => quietSound('hoot')
-      })[loud ? 'loud' : 'quiet']();
+      };
       break;
-    }
   }
+
+  if (actions) {
+    actions[loud ? 'loud' : 'quiet']();
+  }
+}
 
   return {
     makeNoise
