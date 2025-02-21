1afe60f90d76604b71e205066963d5506e1e6b86
Fritz Meissner <fritz.meissner@gmail.com>
2025-02-21 17:05:34 +0000

    Pass objects instead of if (loud) else

--- javascript/noisy_animal.js
+++ javascript/noisy_animal.js
@@ -11,52 +11,48 @@ function createNoisyAnimal(species) {
   function makeNoise({ loud = true } = {}) {
     switch (species) {
     case 'cat':
-      if (loud) {
-        loudSound('meow');
-      } else {
-        quietSound('meow');
-      }
+      ({
+        loud: () => loudSound('meow'),
+        quiet: () => quietSound('meow')
+      })[loud ? 'loud' : 'quiet']();
       break;
 
     case 'dog':
-      if (loud) {
-        loudSound('woof');
-      } else {
-        quietSound('woof');
-      }
+      ({
+        loud: () => loudSound('woof'),
+        quiet: () => quietSound('woof')
+      })[loud ? 'loud' : 'quiet']();
       break;
 
     case 'leopard':
-      if (loud) {
-        loudSound('growl');
-      } else {
-        quietSound('growl');
-      }
+      ({
+        loud: () => loudSound('growl'),
+        quiet: () => quietSound('growl')
+      })[loud ? 'loud' : 'quiet']();
       break;
 
     case 'hadedah':
-      if (loud) {
-        loudSound('squawk');
-      } else {
-        quietSound('squawk');
-        throw new Error('there is no such thing as a quiet hadedah!');
-      }
+      ({
+        loud: () => loudSound('squawk'),
+        quiet: () => {
+          quietSound('squawk');
+          throw new Error('there is no such thing as a quiet hadedah!');
+        }
+      })[loud ? 'loud' : 'quiet']();
       break;
 
     case 'eagle':
-      if (loud) {
-        loudSound('caw');
-      } else {
-        quietSound('caw');
-      }
+      ({
+        loud: () => loudSound('caw'),
+        quiet: () => quietSound('caw')
+      })[loud ? 'loud' : 'quiet']();
       break;
 
     case 'owl':
-      if (loud) {
-        loudSound('hoot');
-      } else {
-        quietSound('hoot');
-      }
+      ({
+        loud: () => loudSound('hoot'),
+        quiet: () => quietSound('hoot')
+      })[loud ? 'loud' : 'quiet']();
       break;
     }
   }
