7c2b81fbaf2a783336b3f194b0526f359aef413f
Fritz Meissner <fritz.meissner@gmail.com>
2025-02-21 16:08:09 +0000

    Remove redundant outer conditional

--- javascript/noisy_animal.js
+++ javascript/noisy_animal.js
@@ -25,29 +25,27 @@ function createNoisyAnimal(species) {
         makeMammalNoise();
       }
     }
-    if (['owl', 'eagle', 'hadedah'].includes(species)) {
-      if (species === 'hadedah') {
-        if (loud) {
-          console.log('squawk');
-          console.log('squawk');
-        } else {
-          console.log('squawk');
-          throw new Error('there is no such thing as a quiet hadedah!');
-        }
-      } else if (species === 'eagle') {
-        if (loud) {
-          console.log('caw');
-          console.log('caw');
-        } else {
-          console.log('caw');
-        }
-      } else if (species === 'owl') {
-        if (loud) {
-          console.log('hoot');
-          console.log('hoot');
-        } else {
-          console.log('hoot');
-        }
+    if (species === 'hadedah') {
+      if (loud) {
+        console.log('squawk');
+        console.log('squawk');
+      } else {
+        console.log('squawk');
+        throw new Error('there is no such thing as a quiet hadedah!');
+      }
+    } else if (species === 'eagle') {
+      if (loud) {
+        console.log('caw');
+        console.log('caw');
+      } else {
+        console.log('caw');
+      }
+    } else if (species === 'owl') {
+      if (loud) {
+        console.log('hoot');
+        console.log('hoot');
+      } else {
+        console.log('hoot');
       }
     }
   }
