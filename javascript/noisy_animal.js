445f677b0742d97dfd100eb1d7a7d9a5052b0166
Fritz Meissner <fritz.meissner@gmail.com>
2025-02-21 16:21:17 +0000

    Expand mammal noise branches to look like bird noises
    We can't DRY up the bird noise branches like the mammal noise branches, so let's
    try it this way.
    
    Question: can we?

--- javascript/noisy_animal.js
+++ javascript/noisy_animal.js
@@ -1,16 +1,27 @@
 function createNoisyAnimal(species) {
   function makeNoise({ loud = true } = {}) {
     if (['cat', 'dog', 'leopard'].includes(species)) {
-      noise = {
-        cat: 'meow',
-        dog: 'woof',
-        leopard: 'growl'
-      }[species];
-      if (loud) {
-        console.log(noise);
-        console.log(noise);
-      } else {
-        console.log(noise);
+      if (species === 'cat') {
+        if (loud) {
+          console.log('meow');
+          console.log('meow');
+        } else {
+          console.log('meow');
+        }
+      } else if (species === 'dog') {
+        if (loud) {
+          console.log('woof');
+          console.log('woof');
+        } else {
+          console.log('woof');
+        }
+      } else if (species === 'leopard') {
+        if (loud) {
+          console.log('growl');
+          console.log('growl');
+        } else {
+          console.log('growl');
+        }
       }
     }
     if (species === 'hadedah') {
