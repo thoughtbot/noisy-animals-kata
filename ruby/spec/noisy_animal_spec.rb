a35d604ccc14e70e6a9dd519aca306277ef3ceb2
Fritz Meissner <fritz.meissner@gmail.com>
2025-03-07 13:59:10 +0000

    Make the tests worse in order to hide meaning
    The code is intended to be terrible and hide meaning so that you have to clean
    it up.
    
    Previously, the tests were too good and gave the reader an out from that
    problem. Now, they are worse :->.

--- ruby/spec/noisy_animal_spec.rb
+++ ruby/spec/noisy_animal_spec.rb
@@ -4,82 +4,74 @@ require 'rspec'
 require_relative '../noisy_animal'
 
 describe NoisyAnimal do
-  context 'when the animal is a cat' do
-    it 'makes a meow noise twice' do
-      animal = NoisyAnimal.new('cat')
+  it 'loud dog' do
+    animal = NoisyAnimal.new('dog')
 
-      expect { animal.make_noise }.to output("meow\nmeow\n").to_stdout
-    end
+    expect { animal.make_noise }.to output("woof\nwoof\n").to_stdout
   end
 
-  context 'when the animal is a dog' do
-    it 'makes a woof noise twice' do
-      animal = NoisyAnimal.new('dog')
+  it 'quiet cat' do
+    animal = NoisyAnimal.new('cat')
 
-      expect { animal.make_noise }.to output("woof\nwoof\n").to_stdout
-    end
+    expect { animal.make_noise(loud: false) }.to output("meow\n").to_stdout
   end
 
-  context 'when the animal is a leopard' do
-    it 'makes a growl noise twice' do
-      animal = NoisyAnimal.new('leopard')
+  it 'loud leopard' do
+    animal = NoisyAnimal.new('leopard')
 
-      expect { animal.make_noise }.to output("growl\ngrowl\n").to_stdout
-    end
+    expect { animal.make_noise }.to output("growl\ngrowl\n").to_stdout
   end
 
-  context 'when the animal is unusually quiet' do
-    it 'cat goes meow' do
-      animal = NoisyAnimal.new('cat')
+  it 'quiet dog' do
+    animal = NoisyAnimal.new('dog')
 
-      expect { animal.make_noise(loud: false) }.to output("meow\n").to_stdout
-    end
+    expect { animal.make_noise(loud: false) }.to output("woof\n").to_stdout
+  end
 
-    it 'dog goes woof' do
-      animal = NoisyAnimal.new('dog')
+  it 'quiet leopard' do
+    animal = NoisyAnimal.new('leopard')
 
-      expect { animal.make_noise(loud: false) }.to output("woof\n").to_stdout
-    end
+    expect { animal.make_noise(loud: false) }.to output("growl\n").to_stdout
+  end
 
-    it 'leopard goes growl' do
-      animal = NoisyAnimal.new('leopard')
+  it 'loud cat' do
+    animal = NoisyAnimal.new('cat')
 
-      expect { animal.make_noise(loud: false) }.to output("growl\n").to_stdout
-    end
+    expect { animal.make_noise }.to output("meow\nmeow\n").to_stdout
   end
 
   context 'when the animal is really a bird' do
-    it 'loud owls hoot twice' do
+    it 'loud owls' do
       animal = NoisyAnimal.new('owl')
 
       expect { animal.make_noise }.to output("hoot\nhoot\n").to_stdout
     end
 
-    it 'loud eagles caw twice' do
+    it 'loud eagles' do
       animal = NoisyAnimal.new('eagle')
 
       expect { animal.make_noise }.to output("caw\ncaw\n").to_stdout
     end
 
-    it 'loud hadedahs squawk twice' do
+    it 'loud hadedahs' do
       animal = NoisyAnimal.new('hadedah')
 
       expect { animal.make_noise }.to output("squawk\nsquawk\n").to_stdout
     end
 
-    it 'quiet owls hoot' do
+    it 'quiet owls' do
       animal = NoisyAnimal.new('owl')
 
       expect { animal.make_noise(loud: false) }.to output("hoot\n").to_stdout
     end
 
-    it 'quiet eagles caw' do
+    it 'quiet eagles' do
       animal = NoisyAnimal.new('eagle')
 
       expect { animal.make_noise(loud: false) }.to output("caw\n").to_stdout
     end
 
-    it 'quiet hadedahs do not exist' do
+    it 'quiet hadedahs' do
       animal = NoisyAnimal.new('hadedah')
 
       expect { animal.make_noise(loud: false) }.to raise_error(/no such thing/)
