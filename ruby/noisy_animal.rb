996092870090acf13981c4133e29a7b29c78a791
Fritz Meissner <fritz.meissner@gmail.com>
2025-08-15 14:02:32 +0100

    Remove unreachable code in conditional that is always false

--- ruby/noisy_animal.rb
+++ ruby/noisy_animal.rb
@@ -59,19 +59,7 @@ class NoisyAnimal
     else
       puts 'hoot'
     end
-    if is_loud
-      if species == 'owl'
-        puts 'hoot'
-      end
-      if species == 'eagle'
-        puts 'caw'
-      end
-      if species == 'hadedah'
-        puts 'squawk'
-      end
-    else
-      raise 'there is no such thing as a quiet hadedah!' if species == 'hadedah'
-    end
+    raise 'there is no such thing as a quiet hadedah!' if species == 'hadedah'
   end
 
   def is_bird
