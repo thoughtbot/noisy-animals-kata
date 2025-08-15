7f8a7c006bf83feb72c73847658881782b086fb9
Fritz Meissner <fritz.meissner@gmail.com>
2025-08-15 13:57:21 +0100

    Introduce make_bird_noise_loud with ugly use of flag

--- ruby/noisy_animal.rb
+++ ruby/noisy_animal.rb
@@ -15,7 +15,7 @@ class NoisyAnimal
       if animal_noise
         2.times { puts animal_noise }
       end
-      make_bird_noise(true) if is_bird
+      make_bird_noise_loud if is_bird
     elsif %w[cat dog leopard].include?(species)
       puts animal_noise
     end
@@ -31,6 +31,29 @@ class NoisyAnimal
     }[species]
   end
 
+  def make_bird_noise_loud
+    is_loud = true
+    if species == 'hadedah'
+      puts 'squawk'
+    elsif species == 'eagle'
+      puts 'caw'
+    else
+      puts 'hoot'
+    end
+    if is_loud
+      if species == 'owl'
+        puts 'hoot'
+      end
+      if species == 'eagle'
+        puts 'caw'
+      end
+      if species == 'hadedah'
+        puts 'squawk'
+      end
+    else
+      raise 'there is no such thing as a quiet hadedah!' if species == 'hadedah'
+    end
+  end
   def make_bird_noise(is_loud = true)
     if species == 'hadedah'
       puts 'squawk'
