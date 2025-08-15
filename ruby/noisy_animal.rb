4add7e24795058c76ff65863d2c789f9c55320b5
Fritz Meissner <fritz.meissner@gmail.com>
2025-08-15 14:28:24 +0100

    Extract function to name and replace boolean expression

--- ruby/noisy_animal.rb
+++ ruby/noisy_animal.rb
@@ -16,7 +16,7 @@ class NoisyAnimal
         2.times { puts animal_noise }
       end
       make_bird_noise_loud if is_bird
-    elsif %w[cat dog leopard].include?(species)
+    elsif is_animal
       puts animal_noise
     end
   end
@@ -63,5 +63,9 @@ class NoisyAnimal
 
   def is_bird
     species == 'owl' || species == 'eagle' || species == 'hadedah'
+  end
+
+  def is_animal
+    %w[cat dog leopard].include?(species)
   end
 end
