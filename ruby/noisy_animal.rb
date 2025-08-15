a6e2a6b49a249085379717530cf66a813164bc6c
Fritz Meissner <fritz.meissner@gmail.com>
2025-08-15 13:59:22 +0100

    Delete obselete make_bird_noise flag function

--- ruby/noisy_animal.rb
+++ ruby/noisy_animal.rb
@@ -79,29 +79,6 @@ class NoisyAnimal
     end
   end
 
-  def make_bird_noise(is_loud = true)
-    if species == 'hadedah'
-      puts 'squawk'
-    elsif species == 'eagle'
-      puts 'caw'
-    else
-      puts 'hoot'
-    end
-    if is_loud
-      if species == 'owl'
-        puts 'hoot'
-      end
-      if species == 'eagle'
-        puts 'caw'
-      end
-      if species == 'hadedah'
-        puts 'squawk'
-      end
-    else
-      raise 'there is no such thing as a quiet hadedah!' if species == 'hadedah'
-    end
-  end
-
   def is_bird
     species == 'owl' || species == 'eagle' || species == 'hadedah'
   end
