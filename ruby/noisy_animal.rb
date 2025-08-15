6ad542dd81ab2a355382d38dde62f010bdb6e492
Fritz Meissner <fritz.meissner@gmail.com>
2025-08-15 13:58:31 +0100

    Introduce ugly flag make_bird_noise_quiet

--- ruby/noisy_animal.rb
+++ ruby/noisy_animal.rb
@@ -9,7 +9,7 @@ class NoisyAnimal
 
   def make_noise(loud: true)
     if is_bird && !loud
-      make_bird_noise(false)
+      make_bird_noise_quiet
     end
     if loud
       if animal_noise
@@ -54,6 +54,31 @@ class NoisyAnimal
       raise 'there is no such thing as a quiet hadedah!' if species == 'hadedah'
     end
   end
+
+  def make_bird_noise_quiet
+    is_loud = false
+    if species == 'hadedah'
+      puts 'squawk'
+    elsif species == 'eagle'
+      puts 'caw'
+    else
+      puts 'hoot'
+    end
+    if is_loud
+      if species == 'owl'
+        puts 'hoot'
+      end
+      if species == 'eagle'
+        puts 'caw'
+      end
+      if species == 'hadedah'
+        puts 'squawk'
+      end
+    else
+      raise 'there is no such thing as a quiet hadedah!' if species == 'hadedah'
+    end
+  end
+
   def make_bird_noise(is_loud = true)
     if species == 'hadedah'
       puts 'squawk'
