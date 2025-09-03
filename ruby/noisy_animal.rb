15dcdbbac2684462432e81060cd1adf8e34e233e
Fritz Meissner <fritz.meissner@gmail.com>
2025-09-03 10:37:32 +0100

    Remove impossible and redundant paths from make_bird_noise_loud

--- ruby/noisy_animal.rb
+++ ruby/noisy_animal.rb
@@ -30,7 +30,6 @@ class NoisyAnimal
   end
 
   def make_bird_noise_loud
-    is_loud = true
     if species == 'hadedah'
       puts 'squawk'
     elsif species == 'eagle'
@@ -38,18 +37,14 @@ class NoisyAnimal
     else
       puts 'hoot'
     end
-    if is_loud
-      if species == 'owl'
-        puts 'hoot'
-      end
-      if species == 'eagle'
-        puts 'caw'
-      end
-      if species == 'hadedah'
-        puts 'squawk'
-      end
-    else
-      raise 'there is no such thing as a quiet hadedah!' if species == 'hadedah'
+    if species == 'owl'
+      puts 'hoot'
+    end
+    if species == 'eagle'
+      puts 'caw'
+    end
+    if species == 'hadedah'
+      puts 'squawk'
     end
   end
 
