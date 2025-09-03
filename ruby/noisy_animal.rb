6cb2de4f7ea3c58f98cd15df59e70ed303aeecc5
Fritz Meissner <fritz.meissner@gmail.com>
2025-09-03 10:35:01 +0100

    Create duplicate functions with preset local vars instead of flags

--- ruby/noisy_animal.rb
+++ ruby/noisy_animal.rb
@@ -29,7 +29,8 @@ class NoisyAnimal
     }[species]
   end
 
-  def make_bird_noise(is_loud = true)
+  def make_bird_noise_loud
+    is_loud = true
     if species == 'hadedah'
       puts 'squawk'
     elsif species == 'eagle'
@@ -50,6 +51,33 @@ class NoisyAnimal
     else
       raise 'there is no such thing as a quiet hadedah!' if species == 'hadedah'
     end
+  end
+
+  def make_bird_noise_quiet
+    is_loud = false
+    if species == 'hadedah'
+      puts 'squawk'
+    elsif species == 'eagle'
+      puts 'caw'
+    else
+      puts 'hoot'
+    end
+    if is_loud
+      if species == 'owl'
+        puts 'hoot'
+      end
+      if species == 'eagle'
+        puts 'caw'
+      end
+      if species == 'hadedah'
+        puts 'squawk'
+      end
+    else
+      raise 'there is no such thing as a quiet hadedah!' if species == 'hadedah'
+    end
+  end
+  def make_bird_noise(is_loud = true)
+    is_loud ? make_bird_noise_loud : make_bird_noise_quiet
   end
 
   def is_bird
