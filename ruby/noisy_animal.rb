965359efa71b2166f3abc5bc46bb78d2799b22a2
Fritz Meissner <fritz.meissner@gmail.com>
2025-09-04 17:24:37 +0100

    Consolidate bird_ animal_ into creature_noise

--- ruby/noisy_animal.rb
+++ ruby/noisy_animal.rb
@@ -9,41 +9,36 @@ class NoisyAnimal
 
   def make_noise(loud: true)
     if species == 'hadedah' && loud
-      2.times { puts bird_noise }
+      2.times { puts creature_noise }
     elsif species == 'hadedah' && !loud
-      puts bird_noise
+      puts creature_noise
       raise 'there is no such thing as a quiet hadedah!'
     elsif is_animal && loud
-      2.times { puts animal_noise }
+      2.times { puts creature_noise }
     elsif is_animal && !loud
-      puts animal_noise
+      puts creature_noise
     elsif is_bird && loud
-      2.times { puts bird_noise }
+      2.times { puts creature_noise }
     elsif is_bird && !loud
-      puts bird_noise
+      puts creature_noise
     end
   end
 
   private
 
-  def animal_noise
+  def creature_noise
     {
       'cat' => 'meow',
       'dog' => 'woof',
-      'leopard' => 'growl'
+      'leopard' => 'growl',
+      'hadedah' => 'squawk',
+      'eagle' => 'caw',
+      'owl' => 'hoot'
     }[species]
   end
 
   def is_animal
     %w[cat dog leopard].include?(species)
-  end
-
-  def bird_noise
-    {
-      'hadedah' => 'squawk',
-      'eagle' => 'caw',
-      'owl' => 'hoot'
-    }[species]
   end
 
   def is_bird
